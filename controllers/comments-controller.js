b3bc3b6ee2966ca8a76d6382a04b23cedad70af9
Neson55 <153004573+Neson55@users.noreply.github.com>
2024-05-14 14:10:35 +0400

    Add files via upload

---
+++ controllers/comments-controller.js
@@ -0,0 +1,57 @@
+const { prisma } = require('../prisma/prisma-client');
+
+const CommentController = {
+  createComment: async (req, res) => {
+    try {
+      const { postId, content } = req.body;
+      const userId = req.user.userId;
+
+      if (!postId || !content) {
+        return res.status(400).json({ error: 'Все поля обязательны' });
+      }
+
+      const comment = await prisma.comment.create({
+        data: {
+          postId,
+          userId,
+          content
+        },
+      });
+
+      res.json(comment);
+    } catch (error) {
+      console.error('Error creating comment:', error);
+      res.status(500).json({ error: 'Не удалось создать комментарий' });
+    }
+
+    res.send('createComment')
+  },
+
+    deleteComment: async (req, res)=> {
+        const {id} = req.params
+        const userId = req.user.userId
+
+        try {
+            const comment = await prisma.comment.findUnique({
+             where: {id}   
+            })
+            if(!comment) {
+                return res.status(404).json({error: "Комментарий не найден"})
+            }
+            if(!comment){
+                return res.status(403).json({error: "Нет доступа"})
+            }
+            await prisma.comment.delete ({
+                where: {id}
+            })
+            res.json(comment)
+            }
+         catch (error) {
+            console.error('Delete comment error', error)
+            
+            res.status(500).json({error: 'Internal server error'})
+        }
+    }
+}
+
+module.exports = CommentController
